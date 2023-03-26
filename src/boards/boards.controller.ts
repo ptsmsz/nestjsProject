cd64f105b5b8186854a682a9cfc798255367fc64
ptsmsz <ptsmsz90@gmail.com>
2023-03-26 18:57:33 +0900

    게시판 등록 service 개발

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
+@ApiTags('Boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
+
+    @Get('/')
+    @ApiOperation({ summary: '게시판 목록 조회 API', description: '게시판 목록 조회를 제공한다.' })
+    getAllBoard(): Board[]{
+        return  this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    @ApiOperation({ summary: '게시판 등록 API', description: '게시판 글쓰기를 제공한다.' })
+    createBoard(
+        @Body('title') title:string,
+        @Body('description') description:string
+    ):Board{
+        return this.boardsService.createBoard(title, description);
+    }
 }
